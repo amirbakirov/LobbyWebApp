36d989d1a17cc1413ddd41773d8a5d2207be686b
amirbakirov <88537474+amirbakirov@users.noreply.github.com>
2024-02-24 20:38:59 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let tg = window.Telegram.WebApp;
 
-document.getElementByID("user_name").value = tg.initDataUnsafe.user.first_name;
-document.getElementByID("user_photo").value = tg.initDataUnsafe.user.photo_url;
-document.getElementByID("user_money").value = "0";
+document.getElementByID("user_name").textContent = tg.initDataUnsafe.user.first_name;
+document.getElementByID("user_photo").src = tg.initDataUnsafe.user.photo_url;
+document.getElementByID("user_money").textContent = "0";
